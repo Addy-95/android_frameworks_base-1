9647b953778cecb41ecea500725efe9c46a29562
xyyx <xyyx@mail.ru>
2018-08-16 06:27:04 +0000

    LockscreenCharging: squashed (1/3)
    needed system/core patch
    based on:
    [2/2] frameworks: show charging current & voltage on lockscreen by yank555-lu
    [2/2]Base: show more battery info on lockscreen when charging by yank555-lu
    LockscreenCharging: Use double for mChargingVoltage and show as4.XV by xyyx
    Lockscreen charging: Formatting improvements by nathanchance
    
    DennySPb: adapt to A11
    
    Change-Id: I42f90b413fd1445ded00cb5769af0ce5faf15c49
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5266,6 +5266,12 @@ public final class Settings {
          */
         public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
+	/**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
