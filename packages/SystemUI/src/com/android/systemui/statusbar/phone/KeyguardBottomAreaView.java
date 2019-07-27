94144415eb4a2284c2a3e8d165839a19242fd2b4
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-07-27 12:26:14 +0200

    SystemUI: Hide Emergency button on bottom lockscreen for everyone
    Change-Id: I0955268324debef97c0e53c6362e87665352c5da
    (cherry picked from commit f8be0b1d44bce6d82408fee694591e7397c2b3bb)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -299,6 +299,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         updateCameraVisibility();
         mKeyguardStateController = Dependency.get(KeyguardStateController.class);
         mKeyguardStateController.addCallback(this);
+        mEmergencyCarrierArea.setVisibility(GONE);
         setClipChildren(false);
         setClipToPadding(false);
         mRightAffordanceView.setOnClickListener(this);
@@ -935,15 +936,11 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         updateWalletVisibility();
         updateControlsVisibility();
 
-        if (dozing) {
-            mOverlayContainer.setVisibility(INVISIBLE);
-            mEmergencyCarrierArea.setVisibility(INVISIBLE);
-        } else {
-            mOverlayContainer.setVisibility(VISIBLE);
-            mEmergencyCarrierArea.setVisibility(VISIBLE);
-            if (animate) {
-                startFinishDozeAnimation();
-            }
+        mOverlayContainer.setVisibility(dozing ? INVISIBLE : VISIBLE);
+        mEmergencyCarrierArea.setVisibility(GONE);
+
+        if (dozing && animate) {
+            startFinishDozeAnimation();
         }
     }
 
