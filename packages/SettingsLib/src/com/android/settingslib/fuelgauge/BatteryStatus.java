iaiaian1/android_frameworks_base

BatteryService: add ChargeTurbo charging support
inspired on dash charge
https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9

Signed-off-by: klozz <klozz@TheXPerienceProject.org>

packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -29,6 +29,7 @@ import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
+import static android.os.BatteryManager.EXTRA_CHARGE_TURBO;
 
 import android.content.Context;
 import android.content.Intent;
@@ -48,6 +49,7 @@ public class BatteryStatus {
     public static final int CHARGING_REGULAR = 1;
     public static final int CHARGING_FAST = 2;
     public static final int CHARGING_DASH = 3;
+    public static final int CHARGING_CHARGE_TURBO = 4;
 
     public final int status;
     public final int level;
@@ -58,11 +60,12 @@ public class BatteryStatus {
     public final int maxChargingWattage;
     public final float temperature;
     public final boolean dashChargeStatus;
+    public final boolean chargeTurboStatus
     public final boolean present;
 
     public BatteryStatus(int status, int level, int plugged, int health,
             int maxChargingCurrent, int maxChargingVoltage,
-            int maxChargingWattage, float temperature, boolean dashChargeStatus, boolean present) {
+            int maxChargingWattage, float temperature, boolean dashChargeStatus, boolean chargeTurboStatus, boolean present) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
@@ -72,6 +75,7 @@ public class BatteryStatus {
         this.maxChargingWattage = maxChargingWattage;
         this.temperature = temperature;
         this.dashChargeStatus = dashChargeStatus;
+		this.chargeTurboStatus = chargeTurboStatus;
         this.present = present;
     }
 
@@ -82,6 +86,7 @@ public class BatteryStatus {
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
         temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
+		chargeTurboStatus = batteryChangedIntent.getBooleanExtra(EXTRA_CHARGE_TURBO, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
         final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
@@ -166,6 +171,7 @@ public class BatteryStatus {
         final int fastThreshold = context.getResources().getInteger(
                 R.integer.config_chargingFastThreshold);
         return dashChargeStatus ? CHARGING_DASH :
+		       chargeTurboStatus ? CHARGING_CHARGE_TURBO :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :

Addy-95/android_frameworks_base-1

Change-Id: I94dc46b6810e31da9aa3093dfcd77d9bff29bd57
Signed-off-by: klozz <klozz@thexperienceproject.org>

packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -29,6 +29,7 @@ import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
 import static android.os.BatteryManager.EXTRA_OEM_FAST_CHARGER;
 import static android.os.BatteryManager.EXTRA_DASH_CHARGER;
+import static android.os.BatteryManager.EXTRA_CHARGE_TURBO;
 
 import android.content.Context;
 import android.content.Intent;
@@ -48,6 +49,7 @@ public class BatteryStatus {
     public static final int CHARGING_REGULAR = 1;
     public static final int CHARGING_FAST = 2;
     public static final int CHARGING_DASH = 3;
+    public static final int CHARGING_CHARGE_TURBO = 4;
 
     public final int status;
     public final int level;
@@ -56,10 +58,11 @@ public class BatteryStatus {
     public final int maxChargingWattage;
     public final boolean oemFastChargeStatus;
     public final boolean dashChargeStatus;
+    public final boolean chargeTurboStatus;
     public final boolean present;
 
     public BatteryStatus(int status, int level, int plugged, int health,
-            int maxChargingWattage, boolean oemFastChargeStatus, boolean dashChargeStatus, boolean present) {
+            int maxChargingWattage, boolean oemFastChargeStatus, boolean dashChargeStatus, boolean chargeTurboStatus, boolean present) {
 
         this.status = status;
         this.level = level;
@@ -68,6 +71,7 @@ public class BatteryStatus {
         this.maxChargingWattage = maxChargingWattage;
         this.oemFastChargeStatus = oemFastChargeStatus;
         this.dashChargeStatus = dashChargeStatus;
+        this.chargeTurboStatus = chargeTurboStatus;
         this.present = present;
     }
 
@@ -78,6 +82,7 @@ public class BatteryStatus {
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
         oemFastChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_OEM_FAST_CHARGER, false);
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
+        chargeTurboStatus = batteryChangedIntent.getBooleanExtra(EXTRA_CHARGE_TURBO, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
         final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
@@ -156,11 +161,17 @@ public class BatteryStatus {
         if (oemFastChargeStatus) {
                 return CHARGING_FAST;
         }
+
+       if (isChargeTurbo) {
+              return CHARGING_FAST;
+        }
+
         final int slowThreshold = context.getResources().getInteger(
                 R.integer.config_chargingSlowlyThreshold);
         final int fastThreshold = context.getResources().getInteger(
                 R.integer.config_chargingFastThreshold);
         return dashChargeStatus ? CHARGING_DASH :
+                chargeTurboStatus ? CHARGING_CHARGE_TURBO :
                 maxChargingWattage <= 0 ? CHARGING_UNKNOWN :
                 maxChargingWattage < slowThreshold ? CHARGING_SLOWLY :
                         maxChargingWattage > fastThreshold ? CHARGING_FAST :