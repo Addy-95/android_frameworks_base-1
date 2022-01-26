a417cc55b1ba0b0e22d1fcaacfc31c71885c0ab3
Addy-95 <skabdullah.95@gmail.com>
2022-01-26 20:58:48 +0530

    PowerManagerService: Send broadcast when going to sleep
    Change-Id: Ic5d1c8d5ed386e3a341edc61f2f72b5e77ec00d2

---
+++ core/java/com/android/internal/util/xperience/content/Intent.java
@@ -0,0 +1,15 @@
+package com.android.internal.util.xperience.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "xperience.intent.action.GO_TO_SLEEP";
+}
