10c2f6cc9672e229446b2cc2a200ad87e5de8380
Addy-95 <skabdullah.95@gmail.com>
2022-01-26 20:05:55 +0530

    base: Add Touch HIDL support
    Change-Id: Ia426571df8822c1a761e0f64b3e8810d78d010f7

--- core/java/com/android/internal/lineage/hardware/LineageHardwareManager.java
+++ core/java/com/android/internal/lineage/hardware/LineageHardwareManager.java
@@ -44,6 +44,11 @@ import vendor.lineage.livedisplay.V2_0.IReadingEnhancement;
 import vendor.lineage.livedisplay.V2_0.ISunlightEnhancement;
 import vendor.lineage.livedisplay.V2_1.IAntiFlicker;
 
+import vendor.lineage.touch.V1_0.IGloveMode;
+import vendor.lineage.touch.V1_0.IKeyDisabler;
+import vendor.lineage.touch.V1_0.IStylusMode;
+import vendor.lineage.touch.V1_0.ITouchscreenGesture;
+
 import java.io.UnsupportedEncodingException;
 import java.lang.IllegalArgumentException;
 import java.lang.reflect.Field;
